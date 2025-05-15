ec1677e2f39c99ec7a3c27306cee13afdcc6ef77
henrique814 <hmendesamorim@gmail.com>
2025-05-14 22:31:00 -0300

    Add files via upload

---
+++ aula18/script.js
@@ -0,0 +1,15 @@
+    document.write("<h2> Olá Mundo!</h2>");
+        
+        document.write("<p>javascript</p>")
+        //window.alert("boo");
+        //document.getElementById('sub').innerHTML='Olá Mundo!'
+        //document.getElementById('resposta').innerHTML = window.confirm('você está gostando da aula');
+        //document.getElementById('resposta').innerHTML ="olá " + window.prompt('Qual é o seu nome?'); 
+        function abrepagina()
+        {
+            window.open("aula18.html", "_blank");
+        }
+        function exibedados()
+        {
+            window.alert("nome digitado foi:  " + document.getElementById("nome").value)
+        }
